84db8bbba2826bada5e77069720c5036ea1c1b86
Cyril <ccrozat@artfx.fr>
2021-01-19 00:05:31 +0100

    Beginning of scenes system

--- Framework/Main.cs
+++ Framework/Main.cs
@@ -14,15 +14,8 @@ namespace Framework
         public int screenSizeX;
         public int screenSizeY;
 
-        public Vector2 mousePosition;
-        public bool mouseLeftClick = false;
-        public bool mouseLeftPress = false;
-
-        public Texture2D backgroundTexture;
-        public Texture2D groundTexture;
-
-        public SpriteControlled player;
-        public Sprite friend;
+        public IDictionary<string, Scene> scenes = new Dictionary<string, Scene>();
+        public Scene currentScene;
 
         private SpriteFont pixeled10;
 
@@ -48,14 +41,12 @@ namespace Framework
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            backgroundTexture = Content.Load<Texture2D>("Sprites/background");
-            groundTexture = Content.Load<Texture2D>("Sprites/ground");
-            player = new SpriteControlled(Content.Load<Texture2D>("Sprites/player"), new Vector2(300, 405), new Vector2(64, 64), true, new Rectangle(20, 15, 23, 47), 5);
-            friend = new Sprite(Content.Load<Texture2D>("Sprites/friend"), new Vector2(400, 405), new Vector2(64, 64), true, new Rectangle(21, 15, 23, 47));
+            scenes.Add("level00" ,new Scene(Content.Load<Texture2D>("Sprites/background"), Content.Load<Texture2D>("Sprites/ground"), graphicsDevice));
+            scenes["level00"].AddSprite("player", new SpriteControlled(Content.Load<Texture2D>("Sprites/player"), new Vector2(300, 405), new Vector2(64, 64), true, new Rectangle(20, 15, 23, 47), 5));
+            scenes["level00"].AddSprite("friend", new Sprite(Content.Load<Texture2D>("Sprites/friend"), new Vector2(400, 405), new Vector2(64, 64), true, new Rectangle(21, 15, 23, 47)));
+            currentScene = scenes["level00"];
 
             pixeled10 = Content.Load<SpriteFont>("Fonts/Pixeled10");
-
-            // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
@@ -65,30 +56,9 @@ namespace Framework
 
             if (IsActive)
             {
-                mousePosition = new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
-                if (mouseLeftClick)
-                {
-                    player.MoveTo(mousePosition.X);
-                }
-
-                player.Update();
-
-                //Mouse Part
-                ButtonState mouseLeftButtonState = Mouse.GetState().LeftButton;
-                if (mouseLeftPress)
-                {
-                    mouseLeftClick = false;
-                }
-                if (mouseLeftButtonState == ButtonState.Pressed && !mouseLeftPress)
-                {
-                    mouseLeftClick = true;
-                    mouseLeftPress = true;
-                }
-                if (mouseLeftButtonState == ButtonState.Released) mouseLeftPress = false;
+                currentScene.Update();
             }
 
-
-            // TODO: Add your update logic here
 
             base.Update(gameTime);
         }
@@ -97,18 +67,7 @@ namespace Framework
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             _spriteBatch.Begin();
-
-            _spriteBatch.Draw(backgroundTexture, new Rectangle(0, 0, screenSizeX, screenSizeY), Color.White);
-            _spriteBatch.Draw(groundTexture, new Rectangle(0, 5 * screenSizeY / 6, screenSizeX, screenSizeY / 6), Color.White);
-            player.Draw(_spriteBatch);
-            if (player.Intersects(friend))
-            {
-                _spriteBatch.DrawString(pixeled10, "Oops, sorry.", new Vector2(player.position.X - player.centerOffsetX - 30, player.position.Y - player.centerOffsetY - 25), Color.Red);
-            }
-            friend.Draw(_spriteBatch);
-
-
-            // TODO: Add your drawing code here
+            currentScene.Draw(_spriteBatch);
             _spriteBatch.End();
 
             base.Draw(gameTime);
---
+++ Framework/Scene.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+namespace Framework
+{
+    public class Scene
+    {
+        private IDictionary<string, Sprite> sprites = new Dictionary<string, Sprite>();
+        private IDictionary<string, SpriteControlled> spritesControlled = new Dictionary<string, SpriteControlled>();
+
+        private Texture2D backgroundTex;
+        private Texture2D groundTex;
+        private int screenSizeX;
+        private int screenSizeY;
+
+        public Vector2 mousePosition;
+        public bool mouseLeftClick = false;
+        public bool mouseLeftPress = false;
+
+        public Scene(Texture2D backgroundTexture, Texture2D groundTexture, GraphicsDevice graphicsDevice)
+        {
+            backgroundTex = backgroundTexture;
+            groundTex = groundTexture;
+            screenSizeX = graphicsDevice.Viewport.Width;
+            screenSizeY = graphicsDevice.Viewport.Height;
+        }
+
+        public void Update()
+        {
+            if(mouseLeftClick)
+                foreach (var player in spritesControlled)
+                    player.Value.MoveTo(mousePosition.X);
+
+            foreach (var player in spritesControlled)
+                player.Value.Update();
+
+
+            //Mouse Part
+            mousePosition = new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
+            ButtonState mouseLeftButtonState = Mouse.GetState().LeftButton;
+            if (mouseLeftPress)
+            {
+                mouseLeftClick = false;
+            }
+            if (mouseLeftButtonState == ButtonState.Pressed && !mouseLeftPress)
+            {
+                mouseLeftClick = true;
+                mouseLeftPress = true;
+            }
+            if (mouseLeftButtonState == ButtonState.Released) mouseLeftPress = false;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(backgroundTex, new Rectangle(0, 0, screenSizeX, screenSizeY), Color.White);
+            spriteBatch.Draw(groundTex, new Rectangle(0, 5 * screenSizeY / 6, screenSizeX, screenSizeY / 6), Color.White);
+            foreach (var sprite in sprites)
+                sprite.Value.Draw(spriteBatch);
+            foreach (var player in spritesControlled)
+                player.Value.Draw(spriteBatch);
+        }
+
+        public void AddSprite(string name, Sprite sprite)
+        {
+            sprites.Add(name, sprite);
+        }
+
+        public void AddSprite(string name, SpriteControlled spriteControlled)
+        {
+            spritesControlled.Add(name, spriteControlled);
+        }
+    }
+}
